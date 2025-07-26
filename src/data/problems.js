9f6ce4a825157044f5637793156a39606393edcc
robertogalbiatti <37090978+robertogalbiatti@users.noreply.github.com>
2025-07-25 23:29:03 -0300

    feat: initial commit for problem list (#1)

---
+++ src/data/problems.js
@@ -0,0 +1,17 @@
+export const problems = [
+  {
+    id: 'two-sum',
+    title: 'Two Sum',
+    difficulty: 'Easy',
+    description: 'Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.',
+    code: `function twoSum(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) return [map.get(complement), i];
+    map.set(nums[i], i);
+  }
+}`,
+    tags: ['Hash Map', 'Array'],
+  },
+];
